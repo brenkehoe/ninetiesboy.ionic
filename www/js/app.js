359d5298c37bca3178d674502cd41112e1a4eabf
Brendan Kehoe <brenkehoe@btopenworld.com>
2017-01-10 15:13:14 +0000

    redirect for auth added

--- www/js/app.js
+++ www/js/app.js
@@ -5,10 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('ninetiesboy', ['ionic', 'ninetiesboy.controllers', /*'ninetiesboy.services', */'firebase'])
+angular.module('ninetiesboy', ['ionic', 'ninetiesboy.controllers','firebase'])
 //.constant('FirebaseUrl', 'https://nineties-boy.firebaseio.com/')
 //.service('rootRef', ['FirebaseUrl', Firebase])
-.run(function($ionicPlatform, $ionicLoading) {
+.run(function($ionicPlatform, $ionicLoading, $firebaseAuth, $location, $rootScope, $state) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -21,36 +21,20 @@ angular.module('ninetiesboy', ['ionic', 'ninetiesboy.controllers', /*'ninetiesbo
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    var auth = $firebaseAuth();
 
-/*    $rootScope.firebaseUrl = firebaseUrl;
-    $rootScope.displayName = null;*/
+    auth.$onAuthStateChanged(function(authData) {
+	  if (authData === null) {
+	    console.log("Not logged in yet");
+	    $state.go('login');
+	  } else {
+	    console.log("Logged in as", authData.uid);
+	    $rootScope.authData = authData;
+	    $state.go('tab.news');
+	  }
+	  // This will display the user's name in our view
+	});
 
- /*   Auth.$onAuth(function(authData){
-    	if(authData) {
-    		console.log("Logged in as:", authData.uid);
-    	} else {
-    		console.log("Logged out");
-            $ionicLoading.hide();
-            $location.path('/login');
-    	}
-    });
-
-    $rootScope.logout = function () {
-        console.log("Logging out from the app");
-        $ionicLoading.show({
-            template: 'Logging Out...'
-        });
-        Auth.$unauth();
-    }*/
-
-
-  /*  $rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
-        // We can catch the error thrown when the $requireAuth promise is rejected
-        // and redirect the user back to the home page
-        if (error === "AUTH_REQUIRED") {
-            $location.path("/login");
-        }
-    });*/
   });
 })
 
@@ -65,32 +49,13 @@ angular.module('ninetiesboy', ['ionic', 'ninetiesboy.controllers', /*'ninetiesbo
   .state('login', {
   	url: '/login',
   	templateUrl: 'templates/login.html',
-  	controller: 'LoginCtrl',
-  	resolve: {
-        // controller will not be loaded until $waitForAuth resolves
-        // Auth refers to our $firebaseAuth wrapper in the example above
-        "currentAuth": ["Auth",
-            function (Auth) {
-                // $waitForAuth returns a promise so the resolve waits for it to complete
-                return Auth.$waitForAuth();
-			}]
-    }
+  	controller: 'LoginCtrl'
   })
   // setup an abstract state for the tabs directive
   .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/tabs.html',
-    resolve: {
-        // controller will not be loaded until $requireAuth resolves
-        // Auth refers to our $firebaseAuth wrapper in the example above
-        "currentAuth": ["Auth",
-            function (Auth) {
-                // $requireAuth returns a promise so the resolve waits for it to complete
-                // If the promise is rejected, it will throw a $stateChangeError (see above)
-                return Auth.$requireAuth();
-  			}]
-    }
+    templateUrl: 'templates/tabs.html'
   })
 
   // Each tab has its own nav history stack:
